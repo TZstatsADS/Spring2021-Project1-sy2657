1ff30c2b1be847eb133a57535d80812095bf4709
sy2657 <60480017+sy2657@users.noreply.github.com>
2021-02-01 19:26:11 -0800

    Update piechart.r

--- lib/piechart.r
+++ lib/piechart.r
@@ -2,13 +2,13 @@
 
 # characteristic: race
 
-r1 = nrow(anes_new_red[anes_new_red$race=='1. White non-Hispanic (1948-2012)',])
-r2 = nrow(anes_new_red[anes_new_red$race=='2. Black non-Hispanic (1948-2012)',])
-r3 = nrow(anes_new_red[anes_new_red$race=='5. Hispanic (1966-2012)',])
-r4 = nrow(anes_new_red[anes_new_red$race=='6. Other or multiple races, non-Hispanic (1968-2012)',])
-r5 = nrow(anes_new_red[anes_new_red$race=='3. Asian or Pacific Islander, non-Hispanic (1966-2012)',])
-r6 = nrow(anes_new_red[anes_new_red$race=='4. American Indian or Alaska Native non-Hispanic (1966-2012)',])
-r7 = nrow(anes_new_red[anes_new_red$race=='7. Non-white and non-black (1948-1964)',])
+r1 = nrow(anes_new1[anes_new1$race=='1. White non-Hispanic (1948-2012)',])
+r2 = nrow(anes_new1[anes_new1$race=='2. Black non-Hispanic (1948-2012)',])
+r3 = nrow(anes_new1[anes_new1$race=='5. Hispanic (1966-2012)',])
+r4 = nrow(anes_new1[anes_new1$race=='6. Other or multiple races, non-Hispanic (1968-2012)',])
+r5 = nrow(anes_new1[anes_new1$race=='3. Asian or Pacific Islander, non-Hispanic (1966-2012)',])
+r6 = nrow(anes_new1[anes_new1$race=='4. American Indian or Alaska Native non-Hispanic (1966-2012)',])
+r7 = nrow(anes_new1[anes_new1$race=='7. Non-white and non-black (1948-1964)',])
 
 race.df <- data.frame(group = c("white", "black", "hispanic", "other/multiple", "asian/pacific islander", "American indian/Alaska native", "non-white and non-black"), value = c(r1, r2, r3, r4,r5,r6,r7))
 
@@ -33,3 +33,20 @@ geom_bar(width = 1, stat = "identity")
 
 pie2 <- reg.bp + coord_polar("y", start=0)+ scale_fill_brewer(palette="Set2")
 pie2
+
+# characteristic: work status
+
+nw1 <- nrow(anes_new3[anes_new3$work=='1. Working now',])
+nw2 <- nrow(anes_new3[anes_new3$work=='2. Temporarily laid off',])
+nw4 <- nrow(anes_new3[anes_new3$work=='4. Unemployed',])
+nw5 <- nrow(anes_new3[anes_new3$work=='5. Retired',])
+nw6 <- nrow(anes_new3[anes_new3$work=='6. Permanently disabled',])
+nw7 <- nrow(anes_new3[anes_new3$work=='7. Homemaker',])
+
+work.df <- data.frame(group = c("working", "temporarily laid off", "unemployed", "retired", "permanently disabled","homemaker"), value = c(nw1, nw2, nw4, nw5, nw6, nw7))
+
+work.bp<- ggplot(work.df, aes(x="", y=value, fill=group))+
+geom_bar(width = 1, stat = "identity")
+
+pie3 <- work.bp + coord_polar("y", start=0)+ scale_fill_brewer(palette="Accent")
+pie3
